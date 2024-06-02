a37f752a09b18daec2ad20080f6399f20096d7db
VleeplPi <vlimaster@yandex.ru>
2024-06-02 21:16:52 +0700

    init project

---
+++ src/main.ts
@@ -0,0 +1,14 @@
+import express from "express";
+// import { db } from "./Config/db.config";
+// import { router } from "./Routes/posts.routes";
+
+const app = express();
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+//routes
+// app.use("/api/v1/posts", router);
+
+//db connection then server connection
+app.listen(3000, () => console.log("Server is listening on port 3000"));
