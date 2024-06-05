412f2230ef56969eca1574f4f6d2bb49195b56aa
drDeepl <Vlimaster@yandex.ru>
2024-06-05 15:33:35 +0700

    fixed error handler

--- src/app.ts
+++ src/app.ts
@@ -2,12 +2,10 @@ import express,  { type Express } from 'express';
 import { userRouter } from './Routes/user.routes';
 import { profileRouter } from './Routes/profile.routes';
 import cors from 'cors';
-import passport from 'passport';
-// import { errorConverter, errorHandler } from './middlewares/error';
 import httpStatus from 'http-status';
 import ApiError from './utils/ApiError';
 import { errors } from './middlewares/error';
-// import { jwtStrategy } from './Config/passport.config';
+
 
 const app: Express = express();
 
@@ -19,16 +17,12 @@ app.use(express.urlencoded({ extended: true }));
 app.use('/api/v1/user', userRouter);
 app.use('/api/v1/profile', profileRouter);
 
-// app.use(passport.initialize());
-// passport.use('jwt', jwtStrategy);
+
 
 app.use((req, res, next) => {
   next(new ApiError(httpStatus.NOT_FOUND, 'Not found'));
 });
 
-// app.use(errorConverter);
-
-// app.use(errorHandler);
 
 app.use(errors);
 
