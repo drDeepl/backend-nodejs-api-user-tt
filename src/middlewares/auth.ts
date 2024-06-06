e5d95ed75673925652d7267f0afa2dc58be32d4f
drDeepl <Vlimaster@yandex.ru>
2024-06-06 15:42:34 +0700

    update readme

--- src/middlewares/auth.ts
+++ src/middlewares/auth.ts
@@ -3,50 +3,11 @@ import httpStatus from 'http-status';
 import ApiError from '../utils/ApiError';
 
 import { NextFunction, Request, Response } from 'express';
-import { User } from '@prisma/client';
-// import { jwtStrategy } from '../Config/passport.config';
-// import { jwtVerify } from '../Config/passport.config';
+
 import { TokenExpiredError, verify } from 'jsonwebtoken';
 import config from '../Config/env.config';
 import UnauthorizedError from '../errors/UnauthorizedError';
 import prisma from '../../prisma';
-import ForbiddenError from '../errors/ForbiddenError';
-
-const verifyCallback = (
-  req: any,
-  resolve: (value?: unknown) => void,
-  reject: (reason?: unknown) => void,
-) => async (err: unknown, user: User | false, info: unknown) => {
-  console.log('verifyCallback');
-
-  if (err || info || !user) {
-    return reject(new ApiError(httpStatus.UNAUTHORIZED, 'Ошибка авторизации'));
-  }
-  req.user = user;
-  // if (req.params.userId !== user.id) {
-  //   return reject(new ApiError(httpStatus.FORBIDDEN, 'Forbidden'));
-  // }
-
-  resolve();
-};
-
-// const auth = (...requiredRights: string[]) => async (
-//   req: Request,
-//   res: Response,
-//   next: NextFunction,
-// ) => {
-//   console.log('auth');
-
-//   return new Promise((resolve, reject) => {
-//     passport.authenticate(
-//       'jwt',
-//       { session: false },
-//       verifyCallback(req, resolve, reject),
-//     )(req, res, next);
-//   })
-//     .then(() => next())
-//     .catch((err) => next(err));
-// };
 
 const auth = async (req: Request, res: Response, next: NextFunction) => {
   console.log('auth user');
